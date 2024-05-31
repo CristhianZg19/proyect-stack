4138fc8ccb10189987f0c85b58c0ab41aad316a8
cristhian <cristhianzapatagaldos@gmail.com>
2024-05-31 15:27:42 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app>
+    <v-card>
+      <v-layout>
+        <NavigationBarVue></NavigationBarVue>
+        <v-main style="height: 100vh"> 
+          
+        </v-main>
+      </v-layout>
+    </v-card>
+    <!-- <AppFooter /> -->
+  </v-app>
+</template>
+
+<script setup>
+import NavigationBarVue from "./components/NavigationBar.vue";
+
+//
+</script>
---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-navigation-drawer expand-on-hover rail>
+    <v-list>
+      <v-list-item
+        :prepend-avatar="avatar"
+        subtitle="administrador@gmail.com"
+        title="Administrador"
+      ></v-list-item>
+    </v-list>
+
+    <v-divider></v-divider>
+
+    <v-list density="compact" nav>
+      <v-list-item
+        prepend-icon="mdi-account-multiple"
+        title="Usuarios"
+        value="shared"
+      ></v-list-item>
+      <v-list-item
+        prepend-icon="mdi-cog"
+        title="Configuración"
+        value="starred"
+      ></v-list-item>
+    </v-list>
+    <template v-slot:append>
+      <div class="pa-2">
+        <v-btn v-tooltip="'Cerrar sessión'" block
+          ><v-icon>mdi-logout</v-icon></v-btn
+        >
+      </div>
+    </template>
+  </v-navigation-drawer>
+</template>
+<script setup >
+import avatar from "@/assets/avatar.png";
+</script>
